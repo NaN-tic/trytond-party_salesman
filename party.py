db501f9d93ec4faeea85be846275c34cfdc80c77
resteve <resteve@zikzakmedia.com>
2013-10-02 19:48:19 +0200

    pyflakes

--- party.py
+++ party.py
@@ -2,13 +2,9 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 from trytond.model import ModelSQL, fields
-from trytond.pool import Pool, PoolMeta
-from trytond.pyson import Eval, Bool
+from trytond.pool import PoolMeta
 
-__all__ = [
-    'PartySalesman',
-    'Party',
-]
+__all__ = ['PartySalesman', 'Party']
 __metaclass__ = PoolMeta
 
 
