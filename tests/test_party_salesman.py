92e07be536636d5a2c804ac5a3c3386b936e0718
resteve <resteve@zikzakmedia.com>
2015-12-01 11:11:40 +0100

    Tests

--- tests/test_party_salesman.py
+++ tests/test_party_salesman.py
@@ -1,25 +1,14 @@
-#!/usr/bin/env python
-# This file is part of party_salesman module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the party_salesman module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class PartySalesmanTestCase(unittest.TestCase):
+class PartySalesmanTestCase(ModuleTestCase):
     'Test Party Salesman module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('party_salesman')
-
-    def test0005views(self):
-        'Test views'
-        test_view('party_salesman')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'party_salesman'
 
 
 def suite():
@@ -27,6 +16,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         PartySalesmanTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
